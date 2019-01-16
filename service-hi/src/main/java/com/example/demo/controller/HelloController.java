526af2bec8c939748acb53671c1d89acb4a8a3f0
huangxin <1377186691@qq.com>
2019-01-16 13:54:37 +0800

    first commit

---
+++ service-hi/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xin.huang
+ * @version v1.0
+ * @date 2018/8/17 12:44
+ */
+@RestController
+public class HelloController {
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam String name) {
+        return "hi "+name+",i am from port:" +port;
+    }
+}
